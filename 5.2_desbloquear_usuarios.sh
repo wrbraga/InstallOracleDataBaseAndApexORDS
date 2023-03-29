2a1cafa11b22b84726c91d2d0faeffb9333f4909
Wesley R Braga <wrbraga@yahoo.com.br>
2023-03-28 23:40:40 -0300

    Unlocked REST api
    Created procedure for unblock access of the web sites for Rest API.

--- 5.2_desbloquear_usuarios.sh
+++ 5.2_desbloquear_usuarios.sh
@@ -26,7 +26,31 @@ sqlplus / as sysdba <<EOF
  ALTER USER apex_public_user IDENTIFIED BY Oracle_2023 ACCOUNT UNLOCK;
  ALTER USER apex_listener IDENTIFIED BY Oracle_2023 ACCOUNT UNLOCK;
  ALTER USER apex_rest_public_user IDENTIFIED BY Oracle_2023 ACCOUNT UNLOCK;
+
  grant all privileges to pdbadmin;
+
+-- Libera o acesso a todos os sites para que consultas Rest
+-- Possam ser feitas
+DECLARE
+  l_principal VARCHAR2(20) := 'APEX_210200';
+BEGIN
+  DBMS_NETWORK_ACL_ADMIN.create_acl (
+    acl          => 'oracle_base_acl.xml', 
+    description  => 'An ACL for access all websites',
+    principal    => l_principal,
+    is_grant     => TRUE, 
+    privilege    => 'connect',
+    start_date   => SYSTIMESTAMP,
+    end_date     => NULL);
+
+  DBMS_NETWORK_ACL_ADMIN.assign_acl (
+    acl         => 'oracle_base_acl.xml',
+    host        => '*', 
+    lower_port  => 80,
+    upper_port  => 9999); 
+
+  COMMIT;
+END;
 EOF
 
 MENSAGEM="Usuários do APEX desbloqueados com ${AMERELO}SUCESSO.${VERDE}.\nAgora execute o script ${VERMELHO}5.3_configuracao_inicial_ORDS.sh${END}. Para preparar a instalação do ORDS."
